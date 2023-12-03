6f084ce5cd7eff6aab4ed94beb41211a084d6e96
Dave Hulbert <dave1010@gmail.com>
2023-12-03 20:14:00 +0000

    Linter fixes, dockblocks, clean up

--- clipea/__main__.py
+++ clipea/__main__.py
@@ -6,11 +6,16 @@ from clipea import router
 
 
 def clipea_main() -> None:
+    """
+    Main function for the Clipea. It checks if the "llm" dependency is installed
+    and then prompts the user for input. It then routes the user's input
+    to the appropriate command in the router.
+    """
     if shutil.which("llm") is None:
         sys.exit('Error: dependency "llm" not found. Run "clipea setup" to install')
 
-    USER_PROMPT = " ".join(sys.argv[1:])
-    router.commands_router(USER_PROMPT)
+    user_prompt = " ".join(sys.argv[1:])
+    router.commands_router(user_prompt)
 
 
 if __name__ == "__main__":
