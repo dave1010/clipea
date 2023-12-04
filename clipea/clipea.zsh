f4272c272161a625f63425651c11a564c1fdfbf6
Dave Hulbert <dave1010@gmail.com>
2023-12-04 13:16:02 +0000

    Fix zsh WIP

--- clipea/clipea.zsh
+++ clipea/clipea.zsh
@@ -7,28 +7,43 @@ if [[ $ZSH_EVAL_CONTEXT != 'toplevel:file' ]]; then
     return 1 2>/dev/null || exit 1
 fi
 
-CLIPEA_TMP_FILE=$(mktemp)
+CLIPEA_ARGS="$@"
 
-# https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh
-CLIPEA_SCRIPT_DIR=$(dirname $(readlink -f ${(%):-%x}))
+# wrap in a fn so we don't polute the shell with vars
+clipea_init() {
+    local CLIPEA_TMP_FILE=$(mktemp)
+    local CLIPEA_PYTHON_INTERNAL
+    local CLIPEA_PATH_INTERNAL
 
-CLIPEA_PYTHON=
+    # Allow overriding clipea location for development
+    # eg CLIPEA_PATH=./clipea source clipea/clipea.zsh
+    if [[ -z $CLIPEA_PATH ]]; then
+        # default to the installed clipea
+        CLIPEA_PATH_INTERNAL=$(builtin which clipea)
+    else
+        CLIPEA_PATH_INTERNAL=$CLIPEA_PATH
+        # only if clipea is overridden then we need to run it with python
+        # allow overriding python too
+        if [[ -z $CLIPEA_PYTHON ]]; then
+            CLIPEA_PYTHON_INTERNAL="$(builtin which python3 || builtin which python)"
+        else
+            CLIPEA_PYTHON_INTERNAL=$CLIPEA_PYTHON
+        fi
+    fi
 
-CLIPEA_PATH=$(builtin which clipea)
+    #echo Debug: $CLIPEA_PYTHON_INTERNAL $CLIPEA_PATH_INTERNAL $CLIPEA_ARGS
 
-# Run clipea from the current dir if possible
-if [[ -f $CLIPEA_SCRIPT_DIR/__main__.py ]]; then
-    CLIPEA_PATH=$CLIPEA_SCRIPT_DIR
-    CLIPEA_PYTHON="$(builtin which python3 || builtin which python)"
-fi
+    # Execute clipea with an environment variable
+    CLIPEA_CMD_OUTPUT_FILE="$CLIPEA_TMP_FILE" $CLIPEA_PYTHON_INTERNAL "$CLIPEA_PATH_INTERNAL" "$CLIPEA_ARGS"
 
-# Execute clipea with an environment variable
-CLIPEA_CMD_OUTPUT_FILE="$CLIPEA_TMP_FILE" $CLIPEA_PYTHON "$CLIPEA_PATH" "$@"
+    # Read the command to be placed on the Zsh command line
+    COMMAND_TO_PLACE=$(< "$CLIPEA_TMP_FILE")
 
-# Read the command to be placed on the Zsh command line
-CLIPEA_COMMAND_TO_PLACE=$(< "$CLIPEA_TMP_FILE")
+    # Place it on the Zsh command line
+    print -rz "$COMMAND_TO_PLACE"
 
-# Place it on the Zsh command line
-print -rz "$CLIPEA_COMMAND_TO_PLACE"
+    rm "$CLIPEA_TMP_FILE"
+}
+clipea_init
 
-rm "$CLIPEA_TMP_FILE"
+unset CLIPEA_ARGS
